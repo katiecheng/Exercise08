9a16954e7148375e8d03db1b350bcf1fd206110b
Hackbright Student <info@hackbrightacademy.com>
2014-06-10 12:41:29 -0700

    Wrote functions to take input text, build a dictionary of Markov chains with it and generate random text

--- markov.py
+++ markov.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import random
 
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
@@ -15,30 +16,42 @@ def make_chains(corpus):
 
     swift_dict = {}
 
-    for i in range(len(list_text)-1):
+    for i in range(len(list_text)-2):
         key = (list_text[i], list_text[i+1])
-        swift_dict[key] = [None]
         # check if key already exists in dict
         # if yes, add value to list of values
-        # if no, add new key:value pair
+        if key in swift_dict:
+            swift_dict[key] += [list_text[i+2]]
+        # if not, add new key:value pair
+        else:
+            swift_dict[key] = [list_text[i+2]]
 
-    print swift_dict
-
-    return {}
+    return swift_dict
 
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
-    return "Here's some random text."
+
+    # for key, value in chains.iteritems():
+
+    random_output = ''
+    current_key = random.choice(chains.keys())
+
+    while current_key in chains.keys():
+        next_word = random.choice(chains[current_key])
+        current_key = (current_key[1], next_word)
+        random_output += " %s" % next_word
+        print random_output
+
+
 
 def main():
     args = sys.argv
-    make_chains("trouble_swift.txt")
-    # Change this to read input_text from a file
-    # input_text = "Some text"
 
-    # chain_dict = make_chains(input_text)
-    # random_text = make_text(chain_dict)
+    # Read input_text from a file
+    input_text = args[1]
+    chain_dict = make_chains(input_text)
+    random_text = make_text(chain_dict)
     # print random_text
 
 if __name__ == "__main__":
