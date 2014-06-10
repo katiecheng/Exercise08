cfa3c7cb7b98e83d1f140d6a3d083d00834729e6
Hackbright Student <info@hackbrightacademy.com>
2014-06-10 16:21:37 -0700

    Code working per ex08 instructions

--- markov.py
+++ markov.py
@@ -7,22 +7,15 @@ def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
     text = open(corpus)
-
-    list_text = []
-
-    for line in text:
-        list_text += line.rstrip().split(" ")
-        # print list_text
+    contents = text.read()
+    list_text = contents.split()
 
     swift_dict = {}
 
     for i in range(len(list_text)-2):
         key = (list_text[i], list_text[i+1])
-        # check if key already exists in dict
-        # if yes, add value to list of values
         if key in swift_dict:
             swift_dict[key] += [list_text[i+2]]
-        # if not, add new key:value pair
         else:
             swift_dict[key] = [list_text[i+2]]
 
@@ -32,27 +25,23 @@ def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
 
-    # for key, value in chains.iteritems():
+    start = random.choice(chains.keys())
+    random_output = start[0] + ' ' + start[1]
+    current_key = start
 
-    random_output = ''
-    current_key = random.choice(chains.keys())
-
-    while current_key in chains.keys():
+    while chains.get(current_key):
         next_word = random.choice(chains[current_key])
         current_key = (current_key[1], next_word)
         random_output += " %s" % next_word
-        print random_output
-
-
+    
+    print random_output
 
 def main():
     args = sys.argv
 
-    # Read input_text from a file
     input_text = args[1]
     chain_dict = make_chains(input_text)
     random_text = make_text(chain_dict)
-    # print random_text
 
 if __name__ == "__main__":
     main()
